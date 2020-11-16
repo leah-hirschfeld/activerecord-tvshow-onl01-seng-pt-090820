99ef772d0b692ff003770e14866cdcc73785d51e
Leah Hirschfeld <leah.hirschfeld@gmail.com>
2020-11-16 02:57:55 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -6,7 +6,7 @@ class Show < ActiveRecord::Base
   end
 
   def self.most_popular_show
-    show = Show.where("rating == #{self.highest_rating}")
+    show = Show.where("rating == #{self.highest_rating}")[0]
     show[0]
   end
 
